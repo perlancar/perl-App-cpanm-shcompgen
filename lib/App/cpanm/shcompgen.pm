e408fa2a40f912c6927887997e20f4af0f76244b
perlancar (on pc-home) <perlancar@gmail.com>
2017-07-08 09:06:16 +0700

    Initial commit

---
+++ lib/App/cpanm/shcompgen.pm
@@ -0,0 +1,21 @@
+package App::cpanm::shcompgen;
+
+# DATE
+# VERSION
+
+1;
+
+# ABSTRACT: Install modules from CPAN (and activate shell completion)
+
+=head1 DESCRIPTION
+
+See the included L<cpanm-shcompgen> script.
+
+
+=head1 SEE ALSO
+
+L<App::cpanminus>
+
+L<App::shcompgen>
+
+=cut
